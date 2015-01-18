c36a93d1814c2d3133fb91c45d63193867aec976
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-01-18 23:07:25 +0100

    Fix namespace error

--- src/ConnectionError.php
+++ src/ConnectionError.php
@@ -6,7 +6,7 @@
 class ConnectionError extends \Exception
 {
 
-    public function __construct($message = null, $code = 0, Exception $previous = null) {
+    public function __construct($message = null, $code = 0, \Exception $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 
