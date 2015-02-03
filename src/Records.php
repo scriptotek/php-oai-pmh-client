scriptotek/php-oai-pmh-client

commit 7b03edbb57d439d7a8e119907646b5de9c077aea

Use 'cursor' value to determine record position
- When resuming a harvest, it makes much more sense to start
  at the real position rather than 1, so we can determine how
  many records there are left to be fetched.

src/Records.php
@@ -30,7 +30,9 @@ class Records extends EventEmitter implements \Iterator {
 	private $lastResponse;
 
 	private $position;
-	private $resumptionToken = null;
+	private $initialPosition;
+	private $resumptionToken;
+	private $initialResumptionToken;
 
     /** @var int Total number of records in the result set */
     public $numberOfRecords;
@@ -53,9 +55,14 @@ class Records extends EventEmitter implements \Iterator {
 		$this->set = $set;
 		$this->client = $client;
 		$this->resumptionToken = $resumptionToken;
+
 		$this->extraParams = $extraParams;
 		$this->position = 1;
 		$this->fetchMore();
+
+		// Store initial position, might not be 1 if we are resuming an operation
+		$this->initialPosition = $this->position;
+		$this->initialResumptionToken = $resumptionToken;
 	}
 
 	/**
@@ -99,6 +106,7 @@ class Records extends EventEmitter implements \Iterator {
 
 		if (isset($this->lastResponse->numberOfRecords) && !is_null($this->lastResponse->numberOfRecords)) {
 			$this->numberOfRecords = $this->lastResponse->numberOfRecords;
+			$this->position = $this->lastResponse->cursor + 1;
 
 		} else if (!isset($this->numberOfRecords)) {
 			$this->numberOfRecords = count($this->lastResponse->records);
@@ -106,6 +114,8 @@ class Records extends EventEmitter implements \Iterator {
 
 		if (isset($this->lastResponse->resumptionToken)) {
 			$this->resumptionToken = $this->lastResponse->resumptionToken;
+		} else {
+			// $this->resumptionToken = null;
 		}
 
 	}
@@ -132,9 +142,9 @@ class Records extends EventEmitter implements \Iterator {
      * Rewind the Iterator to the first element
      */
 	function rewind() {
-		if ($this->position != 1) {
-			$this->position = 1;
-			$this->resumptionToken = null;
+		if ($this->position != $this->initialPosition) {
+			$this->position = $this->initialPosition;
+			$this->resumptionToken = $this->initialResumptionToken;
 			$this->data = array();
 			$this->fetchMore();
 		}

src/ListRecordsResponse.php
@@ -27,7 +27,7 @@ class ListRecordsResponse extends Response {
     /** @var int Total number of records in the result set */
     public $numberOfRecords;
 
-    /** @var int Position of next record in the result set, or null if no such record exist */
+    /** @var int Position of the first record in the response relative to the result set (starts at 0) */
     public $cursor;
 
     /** @var int Token for retrieving more records */
@@ -56,8 +56,8 @@ class ListRecordsResponse extends Response {
 
             $r = $main->first('oai:resumptionToken') ?: null;
             if ($r) {
-                $this->numberOfRecords = intval($r->attr('completeListSize'));
-                $this->cursor = intval($r->attr('cursor'));
+                $this->numberOfRecords = ($r->attr('completeListSize') === '') ? null : intval($r->attr('completeListSize'));
+                $this->cursor = ($r->attr('cursor') === '') ? null : intval($r->attr('cursor'));
                 $this->resumptionToken = $r->text();
             } else {
                 $this->resumptionToken = null;            

commit 0c229c9aa8058784191551baef677b2a37daf6c9

Fix neverending harvest bug introduced in 7b03edb

src/Records.php
@@ -142,7 +142,7 @@ class Records extends EventEmitter implements \Iterator {
 		if (isset($this->lastResponse->resumptionToken)) {
 			$this->resumptionToken = $this->lastResponse->resumptionToken;
 		} else {
-			// $this->resumptionToken = null;
+			$this->resumptionToken = null;
 		}
 
 	}